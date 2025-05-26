73b68785ac62672aa7c35a72b53371e19953e1e2
Saquib Mahfuz <saquibmahfuz3579@gmail.com>
2025-05-26 19:18:30 +0530

    RICR

---
+++ module/module.py
@@ -0,0 +1,14 @@
+def sum (* temp):
+    x=0
+    for i in temp: 
+        x+=i
+    return x
+def sub(a,b):
+    return a-b
+# print(__name__)
+if __name__=='__main__':
+
+    print(sum(3,4,5,6))
+    print(sub(5,3))
+
+age =59
---
+++ module/test.py
@@ -0,0 +1,6 @@
+import module  # import: import whole file 
+
+print(module.sum(4,5))
+from module import sum,age # import the particular function from the file 
+print(sum(9,9))
+print(age)
